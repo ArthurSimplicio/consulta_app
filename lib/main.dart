266dda04498f304de8352cb37f5f5b0e146ecfb7
ArthurSimplicio <arthurpark20@gmail.com>
2023-11-29 23:50:42 -0300

    Atualizado

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ import 'package:consulta_app/pages/main_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:intl/date_symbol_data_file.dart';
 import 'firebase_options.dart';
-import 'package:intl/intl.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,8 +1,6 @@
-
-
 // ignore_for_file: file_names
 
-import "package:consulta_app/pages/time.dart";
+import "package:awesome_dialog/awesome_dialog.dart";
 import "package:consulta_app/utils/doctors.dart";
 import "package:flutter/material.dart"; // Certifique-se de importar corretamente
 
@@ -14,51 +12,109 @@ class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(doctor.nome),
-          ),
-          body: Center(
-            child: Column(
-              children: <Widget>[
-                Container(
-                  height: 300,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(doctor.nome),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.asset(
+                  doctor.foto,
+                  height: 200,
                   width: double.infinity,
-                  child: Image.asset(
-                    doctor.foto,
-                    height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),              
-                Container(padding: EdgeInsets.only(top: 20, bottom: 20), color: Colors.grey.shade300, child: Column(children: [
-                  Text(doctor.nome, textAlign: TextAlign.center, style: const TextStyle(fontSize: 19, fontWeight: FontWeight.w400),),
-                  SizedBox(height: 8,),
-                  Text(doctor.desc, textAlign: TextAlign.center, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),),
-                SizedBox(height: 10,),
-                Container(
-                  alignment: Alignment.center,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('Tarde', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.green),),
-                      SizedBox(width: 30,),
-                      Text('Noite', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: Colors.red),),
-                    ],
-                  ),
-                )
-                ])),
-                FloatingActionButton.extended(onPressed: () {
-                   Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Time(),
-                  ));
-                }, label: Text('Marcar consulta'))
-                
-              ],
-            ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 20, bottom: 20),
+                  color: Colors.grey.shade300,
+                  child: Column(children: [
+                    Text(
+                      doctor.nome,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 19, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      doctor.desc,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Tarde',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.green),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Text(
+                            'Noite',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.red),
+                          ),
+                        ],
+                      ),
+                    )
+                  ])),
+              FloatingActionButton.extended(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => AlertDialog(
+                        title: Text('Concluir agendamento'),
+                        content: Text('Tem certeza?'),
+                        actions: [
+                          TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Não')),
+                          TextButton(
+                              onPressed: () {
+                                AwesomeDialog(
+                                  context: context,
+                                  dialogType: DialogType.success,
+                                  animType: AnimType.bottomSlide,
+                                  title: "Sucesso",
+                                  desc:
+                                      '${doctor.nome} estará lhe esperando próxima quinta',
+                                  btnOkOnPress: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ).show();
+                                
+                              },
+                              child: Text('Sim')),
+                        ],
+                      ),
+                    ));
+                  },
+                  label: Text('Marcar consulta'))
+            ],
           ),
         ),
+      ),
     );
   }
 }
